1a4e93128b937aef2d80cea16b3af8ac3e6f9c62
Aderbal Filho <aderbalcrato@yahoo.com.br>
2020-09-11 18:24:31 -0300

    section 2 lessons 26-27

---
+++ second-section/lessons 26-27/index.js
@@ -0,0 +1,28 @@
+new Vue({
+  el: '#app',
+  data: {
+    counter: 0,
+    secondCounter: 0
+  },
+  computed: {
+    output() {
+      console.log('Computed');
+      return this.counter > 5 ? 'Greater 5' : 'Smaller 5';
+    }
+  },
+  watch: {
+    counter(value) {
+      const vm = this;
+
+      setTimeout(() => {
+        vm.counter = 0;
+      }, 2000);
+    }
+  },
+  methods: {
+    result() {
+      console.log('Method');
+      return this.counter > 5 ? 'Greater 5' : 'Smaller 5';
+    }
+  }
+});
