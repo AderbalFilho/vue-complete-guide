ad60882c3b41242c3adace7217826d5ed807255f
Aderbal Filho <aderbalcrato@yahoo.com.br>
2020-09-11 19:15:30 -0300

    Section 2 lessons 29-31

---
+++ second-section/lessons 29-31/index.js
@@ -0,0 +1,15 @@
+new Vue({
+  el: '#app',
+  data: {
+    attachRed: false,
+    color: 'green'
+  },
+  computed: {
+    divClasses() {
+      return {
+        red: this.attachRed,
+        blue: !this.attachRed
+      }
+    }
+  }
+});
