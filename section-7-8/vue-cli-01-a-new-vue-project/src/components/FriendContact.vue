bd174295ebf99bbd63b19e67b4a47f8d4db238fc
Aderbal Filho <aderbalcrato@yahoo.com.br>
2020-09-25 18:00:57 -0300

    section 8 lessons 89-94

---
+++ section-7-8/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,55 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFavorite ? '(Favorite)' : '' }}</h2>
+    <button @click="toggleFavorite">Toggle Favorite</button>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong> {{ phone }}
+      </li>
+      <li>
+        <strong>Email:</strong> {{ email }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    phone: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+    },
+  },
+  data() {
+    return {
+      detailsAreVisible: false,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      this.$emit('toggle-favorite', this.id);
+    }
+  }
+};
+</script>
