7a73a8ad0f09a75e387d35eed88e756dc9ba32a9
Aderbal Filho <aderbalcrato@yahoo.com.br>
2020-09-28 18:59:37 -0300

    exercise 6

---
+++ exercises/vue-3/exercise-6/cmp-communication-assignment-problem/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <user-data
+      @update-age="updateAge"
+      @update-user="updateUser"
+    >
+    </user-data>
+    <active-user :age="age" :username="username"></active-user>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      age: 30,
+      username: 'Aderbal Filho',
+    }
+  },
+  methods: {
+    updateAge(age) {
+      this.age = +age;
+    },
+    updateUser(username) {
+      this.username = username;
+    },
+  }
+}
+</script>
---
+++ exercises/vue-3/exercise-6/cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    <div>
+      <label for="username">Username:</label>
+      <input type="text" id="username" v-model="username" />
+    </div>
+    <div>
+      <label for="age">Age:</label>
+      <input type="number" id="age" v-model="age" />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  emits: [
+    'update-age',
+    'update-user',
+  ],
+  data() {
+    return {
+      age: '',
+      username: '',
+    }
+  },
+  watch: {
+    age(value) {
+      this.$emit('update-age', value)
+    },
+    username(value) {
+      this.$emit('update-user', value)
+    },
+  }
+}
+</script>
