6429073d964e919a85ad0271f485b25ce39b70c6
Aderbal Filho <aderbalcrato@yahoo.com.br>
2020-10-02 18:21:25 -0300

    section 9 lessons 106-113

---
+++ section-9/cmp-adv-01-starting-setup/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <TheHeader />
+  </div>
+</template>
+
+<script>
+import BadgeList from './components/BadgeList';
+import CourseGoals from './components/CourseGoals';
+import TheHeader from './components/TheHeader';
+import UserInfo from './components/UserInfo';
+
+export default {
+  components: {
+    BadgeList,
+    CourseGoals,
+    TheHeader,
+    UserInfo,
+  },
+  data() {
+    return {
+      activeUser: {
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
