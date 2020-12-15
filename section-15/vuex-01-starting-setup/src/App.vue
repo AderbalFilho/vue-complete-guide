659cbf474e7a2c122af9523c92a5183bf938b829
Aderbal Filho <aderbalcrato@yahoo.com.br>
2020-12-15 19:04:43 -0300

    section 15

---
+++ section-15/vuex-01-starting-setup/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <base-container title="Vuex" v-if="isAuth">
+    <TheCounter />
+    <FavoriteValue />
+    <button @click="addTen">Add 10</button>
+    <ChangeCounter />
+  </base-container>
+  <base-container title="Auth">
+    <UserAuth />
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter';
+import ChangeCounter from './components/ChangeCounter';
+import FavoriteValue from './components/FavoriteValue';
+import UserAuth from './components/UserAuth';
+
+export default {
+  components: {
+    BaseContainer,
+    TheCounter,
+    ChangeCounter,
+    FavoriteValue,
+    UserAuth
+  },
+  computed: {
+    isAuth() {
+      return this.$store.getters.userIsAuthenticated;
+    }
+  },
+  methods: {
+    addTen() {
+      // this.$store.dispatch('increase', { value: 10 });
+      this.$store.dispatch({
+        type: 'numbers/increase',
+        value: 10
+      });
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
